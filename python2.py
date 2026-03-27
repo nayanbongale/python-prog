2a22a1d7c81737b5672409caa74d0f69db36fe30
nayanbongale <bongalenayan01@gmail.com>
2026-03-27 09:16:40 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,98 @@
+
+
+#  mylist=["prashant","ashish","komal","ankush","Ashish",77,"sandip",60.52,"prashant"]
+# print(mylist)
+
+# print(type(mylist))
+# print(mylist[0])
+# print(mylist[2])
+# print(mylist[-1])
+# print(mylist[2:5])
+# print(mylist[:5])
+# print(mylist[1:])
+# print(mylist[1:8:2])
+# print(mylist[:])
+# print(mylist[::-1])
+
+
+# mylist[2]="akshay"
+# print(mylist)mylist=["prashant","ashish","komal","ankush","Ashish",77,"sandip",60.52,"prashant"]
+# print(mylist)
+
+# if "ankush" in mylist:
+#   print("yes ankush is awailable")
+# else:
+#   print("not available")
+
+# mylist=["prashant","ashish","komal","ankush","Ashish",77,"sandip",60.52,"prashant"]
+# print(mylist)
+# mylist.append("harsh")
+# mylist.append("laxman")
+# print(mylist)
+
+# mylist.insert(1,"sanket")
+# print(mylist)
+
+# newlist=mylist.copy()
+# print(mylist)
+# print(newlist)
+
+
+# mylist=[['nayan','bongale'],['85.56'],[4400022,"yyy"]]
+# print("example of multidimentional list:")
+
+# print(mylist)
+# # print(mylist[row][col])
+# print(mylist[0][0])
+# print(mylist[0][1])
+# print(mylist[1][0])
+# print(mylist[2][0])
+# print(mylist[2][1])
+
+# list1=["nayan","bongale"]
+# print(list1*3)
+
+# list2=[50,25.50]
+# print(list1+list2)
+
+# list1=["nayan","bongale"]
+# del list1[0]
+# print(list1)
+# del list1
+# print(list1)
+
+# list3=[50,25.50,'nayan']
+# list3.clear()
+# print(list3)
+
+
+# name="nayan"
+# print(name)
+# myname=list(name)
+# print(myname)
+
+# mylist=[40,30,20,10]
+# mylist.reverse()
+# print(mylist)
+
+
+# mylist=[44,22,77,0,9,88]
+# mylist.sort(reverse=True)
+# print(mylist)
+
+
+# mylist=[44,22,77,0,9,88]
+# newlist=mylist
+# print(id(mylist))
+# print(id(newlist))
+
+
+# print('z' in "prashant")
+# print('y' not in "prashant")
+
+# a=100 
+# b=10
+# print(a is b) #true
+# print(a is not b) #false
+
+
